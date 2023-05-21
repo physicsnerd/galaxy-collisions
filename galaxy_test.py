a2a5a7ecfad9942225f46c14758ee02896117133
Auden <auden.nerd@gmail.com>
2023-05-21 09:46:03 -0700

    added notes to tests that need to be expanded, pylint

--- galaxy_test.py
+++ galaxy_test.py
@@ -3,10 +3,12 @@ import numpy as np
 import galaxy as g
 
 class GalaxyTests(unittest.TestCase):
+    """Tests for galaxy.py module, including Galaxy class methods and non-affiliated methods."""
 
     def test_rand_type_initial_type_ratio_0(self):
-        # Test rand_type with initial_type_ratio = 0
-        # The result should always be 'spiral'
+        """Test rand_type with initial_type_ratio = 0
+        The result should always be 'spiral'
+        """
 
         initial_type_ratio = 0
         num_tests = 50
@@ -16,8 +18,8 @@ class GalaxyTests(unittest.TestCase):
             self.assertEqual(galaxy_type, 'spiral')
 
     def test_rand_type_initial_type_ratio_1(self):
-        # Test rand_type with initial_type_ratio = 1
-        # The result should always be 'elliptical'
+        """Test rand_type with initial_type_ratio = 1
+        The result should always be 'elliptical'"""
 
         initial_type_ratio = 1
         num_tests = 50
@@ -27,24 +29,16 @@ class GalaxyTests(unittest.TestCase):
             self.assertEqual(galaxy_type, 'elliptical')
 
     def test_rand_type(self):
-        # Test the rand_type function
-        # Check if the returned galaxy type is either 'elliptical' or 'spiral'
+        """Test the rand_type function
+        Check if the returned galaxy type is either 'elliptical' or 'spiral'"""
 
-        # Test case 1: initial_type_ratio = 0.0
-        galaxy_type = g.rand_type(0.0)
-        self.assertIn(galaxy_type, ['elliptical', 'spiral'])
-
-        # Test case 2: initial_type_ratio = 1.0
-        galaxy_type = g.rand_type(1.0)
-        self.assertIn(galaxy_type, ['elliptical', 'spiral'])
-
-        # Test case 3: initial_type_ratio = 0.5
+        #update to repeatedly run the test and compare with desired ratio
         galaxy_type = g.rand_type(0.5)
         self.assertIn(galaxy_type, ['elliptical', 'spiral'])
 
-    def test_gravity_force(self):
-        # Test the gravity_force function
-        # Check if the returned force components are floats
+    def test_gravity_force(self):#add more tests. add G? make dimensionless?
+        """Test the gravity_force function
+        Check if the returned force components are floats"""
 
         galaxy_one = g.Galaxy(1, 0, 0, 100, 'elliptical')
         galaxy_two = g.Galaxy(2, 3, 4, 200, 'spiral')
@@ -54,23 +48,24 @@ class GalaxyTests(unittest.TestCase):
         self.assertIsInstance(force_y, float)
 
     def test_elliptical_ratio(self):
-        # Test the elliptical_ratio function
-        # Check if the returned ratio is within the range [0, 1]
+        """Test the elliptical_ratio function
+        Check if the returned ratio is within the range [0, 1]"""
 
         galaxy_list = [
             g.Galaxy(1, 0, 0, 100, 'elliptical'),
             g.Galaxy(2, 1, 1, 200, 'spiral'),
             g.Galaxy(3, 2, 2, 300, 'elliptical')
         ]
-        universe_size = 1000
 
         ratio = g.elliptical_ratio(galaxy_list)
 
         self.assertGreaterEqual(ratio, 0)
         self.assertLessEqual(ratio, 1)
 
-    def test_elliptical_ratio_example(self):#add more tests of elliptical ratio!!
-        # Test elliptical_ratio function
+    def test_elliptical_ratio_example(self):#add more tests of dif vals elliptical ratio!!
+        """Test elliptical_ratio function with specific example
+        Checks only counts active galaxies."""
+        #want to also check visibility: negative positions, positive too large positions
 
         # Create a list of galaxies
         galaxies = [
@@ -92,16 +87,18 @@ class GalaxyTests(unittest.TestCase):
 
         # Check if the calculated ratio matches the expected ratio
         self.assertAlmostEqual(ratio, expected_ratio, places=4)
-    
-    def test_distance_x(self):
+
+    def test_distance_x(self):#confirm sign behavior makes sense
+        """Tests distance_x function with specific example."""
         galaxy_one = g.Galaxy(0, 0, 0, 10, 'spiral')
         galaxy_two = g.Galaxy(0, 20, 10, 10, 'elliptical')
 
         x_dist = galaxy_one.distance_x(galaxy_two)
 
         self.assertEqual(x_dist, 20)
-    
-    def test_distance_y(self):
+
+    def test_distance_y(self):#confirm sign behavior makes sense.
+        """Tests distance_y function with specific example."""
         galaxy_one = g.Galaxy(0, 0, 0, 10, 'spiral')
         galaxy_two = g.Galaxy(0, 20, 20, 10, 'elliptical')
 
@@ -109,7 +106,8 @@ class GalaxyTests(unittest.TestCase):
 
         self.assertEqual(y_dist, 20)
 
-    def test_distance(self):
+    def test_distance(self):#confirm sign behavior makes sense wrt gravity function.
+        """Tests euclidean distance function with specific example (3-4-5 triangle)."""
         galaxy_one = g.Galaxy(0, 3, 0, 10, 'spiral')
         galaxy_two = g.Galaxy(0, 0, 4, 10, 'elliptical')
 
@@ -118,6 +116,8 @@ class GalaxyTests(unittest.TestCase):
         self.assertEqual(dist, 5)
 
     def test_angle(self):
+        """Test angle function in case of desired positive, negative, zero result.
+        Uses 3-4-5 triangle."""
         # Test angle function
 
         # Test case 1: Positive x and y coordinates
@@ -132,7 +132,6 @@ class GalaxyTests(unittest.TestCase):
         self.assertAlmostEqual(angle1, expected_angle1, places=4)
 
         # Test case 2: Negative x and y coordinates
-        x2, y2 = -5, -12
         galaxy_three = g.Galaxy(0, -5, 0, 10, 'elliptical')
         galaxy_four = g.Galaxy(0, 0, -12, 10, 'spiral')
         expected_angle2 = np.deg2rad(-67.380136)
@@ -153,29 +152,35 @@ class GalaxyTests(unittest.TestCase):
 
         # Check if the calculated angle matches the expected angle
         self.assertAlmostEqual(angle3, expected_angle3, places=4)
-    
+
     def test_visible_true(self):
+        """Test visible function works when it should be true."""
         galaxy = g.Galaxy(0, 50, 50, 10, 'spiral')
         universe_size = 1000
         is_visible = galaxy.visible(universe_size)
 
         self.assertTrue(is_visible)
-    
+
     def test_visible_not_true_negative(self):
+        """Test visible function works when it should be false because galaxy position negative."""
         galaxy = g.Galaxy(0, -100, -20, 10, 'spiral')
         universe_size = 1000
         is_visible = galaxy.visible(universe_size)
 
         self.assertFalse(is_visible)
-    
+
     def test_visible_not_true_positive(self):
+        """Test visible function works when it should be false because galaxy position too big."""
         galaxy = g.Galaxy(0, 1250, 100, 10, 'spiral')
         universe_size = 1000
         is_visible = galaxy.visible(universe_size)
 
         self.assertFalse(is_visible)
-    
+
     def test_collide_bools(self):
+        """Test collision function appropriately modifies galaxy attributes.
+        1. Check appropriate galaxy made inactive.
+        2. Check type of new galaxy elliptical."""
         # Test collide function
 
         # Test case 1: Colliding galaxies
@@ -188,14 +193,16 @@ class GalaxyTests(unittest.TestCase):
         # Check if the collided galaxies are no longer active
         self.assertTrue(galaxy1.active)
         self.assertFalse(galaxy2.active)
-        
+
         # Check the resulting galaxy is elliptical
         self.assertEqual(galaxy1.gal_type, 'elliptical')
-    
+
     def test_collide_momentum(self):
+        """Test collision function obeys conservation of momentum."""
         return
-    
+
     def test_time_update(self):
+        """Test time update function."""
         return
 
 if __name__ == '__main__':
